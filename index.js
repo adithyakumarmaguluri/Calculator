009780d8ed801a2502d11f69e4a2b18f1c227b94
adithyakumarmaguluri <138872098+adithyakumarmaguluri@users.noreply.github.com>
2023-07-07 16:29:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var clickSound=new Audio()
+clickSound.src="./Click Sound.wav"
+function clickAudio()
+{
+    clickSound.play();
+}
+var clickSound1=new Audio()
+clickSound1.src="./ClickSound1.wav"
+function clickAudio1()
+{
+    clickSound1.play();
+}
+var clickSound2=new Audio()
+clickSound2.src="./ClickSound2.wav"
+function clickAudio2()
+{
+    clickSound2.play();
+}
